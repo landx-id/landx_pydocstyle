bcd2822916964ef102db6f79e7408e59deb748b3
mashanz <mangatkk@gmail.com>
2022-07-01 08:50:41 +0700

    [FIX] miss counting single file

--- src/pydocstyle/cli.py
+++ src/pydocstyle/cli.py
@@ -80,6 +80,7 @@ def run_pydocstyle():
 
     if run_conf.max_error_percentage:
         print("=" * 80)
+        error_files = 1 if count > 0 and error_files == 0 else error_files
         calculate_percentage = error_files/total_file*100
         print(f"Error Files {error_files} of {total_file}")
         if calculate_percentage < float((run_conf.max_error_percentage)):
